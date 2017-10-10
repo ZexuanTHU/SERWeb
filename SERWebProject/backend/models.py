87ec22a1df85e6da35ea69b07b2e90df400c869d
叶泽轩 <ye-zx14@mails.tsinghua.edu.cn>
2017-10-10 14:45:34 +0800

    add view 'projreg' and config urls

--- SERWebProject/backend/models.py
+++ SERWebProject/backend/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
@@ -10,8 +12,14 @@ class Project(models.Model):
     def __str__(self):
         return self.project_name
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=10)
     votes = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice_text
--- SERWebProject/backend/urls.py
+++ SERWebProject/backend/urls.py
@@ -2,5 +2,11 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^hello', views.hello, name='hello')
+    url(r'^hello', views.hello, name='hello'),
+    url(r'^$', views.projreg, name='projreg'),
+    url(r'^(?P<project_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<project_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<project_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
