6f6bd903656913c81573d718a76c3eb59ab5e4c3
叶泽轩 <ye-zx14@mails.tsinghua.edu.cn>
2017-10-10 11:23:23 +0800

    add model 'Project' and config setting.py

--- SERWebProject/backend/models.py
+++ SERWebProject/backend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Project(models.Model):
+    project_name = models.CharField(max_length=30)  # 项目名称
+    project_text = models.CharField(max_length=200)  # 项目描述
+    pub_date = models.DateTimeField('date published')  # 发布时间
+
+    def __str__(self):
+        return self.project_name
