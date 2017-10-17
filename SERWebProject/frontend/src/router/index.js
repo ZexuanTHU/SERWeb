e1958279feeded893b38dfd8ecde73651f18e306
温家尊 <jiazunvoon@hotmail.com>
2017-10-17 15:03:57 +0800

    Update index.js

--- SERWebProject/frontend/src/router/index.js
+++ SERWebProject/frontend/src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import Element from 'element-ui'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
+Vue.use(Element)
 
 export default new Router({
   routes: [
