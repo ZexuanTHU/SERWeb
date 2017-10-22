9e2ae64e0e0e337deacbd114c33a3489e7e04a2d
王斯右 <joey840404@gmail.com>
2017-10-22 19:44:16 +0800

    add competition Info page framework #19

---
+++ SERWebProject/frontend/src/components/CompetitionInfo/CompetitionInfo.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div id="nav">
+      <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-menu-item index="1">首页</el-menu-item>
+        <el-submenu index="2">
+          <template slot="title">项目列表</template>
+          <el-menu-item index="2-1">项目报名</el-menu-item>
+          <el-menu-item index="2-2">比赛日程</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">历史沿革</a></el-menu-item>
+        <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">院系荣誉</a></el-menu-item>
+        <el-menu-item index="5"><a href="https://www.ele.me" target="_blank">突出个人</a></el-menu-item>
+        <el-menu-item index="6"><a href="https://www.ele.me" target="_blank">加入院队</a></el-menu-item>
+        <el-menu-item id="login" index="7">
+          <router-link to="Login">用户信息</router-link>
+        </el-menu-item>
+      </el-menu>
+    </div>
+
+    <div>
+      <h1 align="left">赛事资讯</h1>
+      <ul id="info">
+        <li v-for="item in items">
+          {{ item.message }}
+        </li>
+      </ul>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import ElRow from "element-ui/packages/row/src/row";
+
+export default {
+  el: '#info',
+  data: {
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
+  }
+}
+</script>
