a2a7c9b538c20b3e890dfd90b5be5754135c9f2e
王斯右 <joey840404@gmail.com>
2017-10-25 21:35:29 +0800

    Add register form pop up details, using rule form #19

--- SERWebProject/frontend/src/components/CompetitionInfo/CompetitionInfo.vue
+++ SERWebProject/frontend/src/components/CompetitionInfo/CompetitionInfo.vue
@@ -22,7 +22,10 @@
       <h1 align="left">{{name}}</h1>
       <div id="basic">
         <p align="left">
-          时间: {{time}}
+          比赛时间: {{competitionTime}}
+        </p>
+        <p align="left">
+          报名时间: {{registerTime}}
         </p>
         <p align="left">
           地点: {{location}}
@@ -45,21 +48,48 @@
       </div>
       <el-button id="submit" @click="dialogFormVisible = true" type="primary">立即报名</el-button>
       <el-dialog title="报名资料填写" :visible.sync="dialogFormVisible">
-        <el-form :model="form">
-          <el-form-item label="名字" :label-width="formLabelWidth">
-            <el-input v-model="form.name" auto-complete="off"></el-input>
-          </el-form-item>
-          <el-form-item label="项目" :label-width="formLabelWidth">
-            <el-select v-model="form.region" placeholder="请选择报名项目">
-              <el-option label="半程" value="half"></el-option>
-              <el-option label="全程" value="whole"></el-option>
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+          <el-form-item label="院系" prop="department">
+            <el-select v-model="ruleForm.department" placeholder="选择院系">
+              <el-option label="计算机" value="cs"></el-option>
+              <el-option label="自动化" value="auto"></el-option>
+              <el-option label="电子" value="ee"></el-option>
             </el-select>
           </el-form-item>
+          <el-form-item label="名字" prop="name">
+            <el-input v-model="ruleForm.name"></el-input>
+          </el-form-item>
+          <el-form-item label="性别" prop="gender">
+            <el-select v-model="ruleForm.gender" placeholder="选择性别">
+              <el-option label="男" value="male"></el-option>
+              <el-option label="女" value="female"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="证件号码" prop="idNumber">
+            <el-input v-model="ruleForm.idNumber"></el-input>
+          </el-form-item>
+          <el-form-item label="学号" prop="studentId">
+            <el-input v-model="ruleForm.studentId"></el-input>
+          </el-form-item>
+          <el-form-item prop="birth">
+            <el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.birth" style="width: 100%;"></el-date-picker>
+          </el-form-item>
+          <el-form-item label="服装号码" prop="clothSize">
+            <el-select v-model="ruleForm.clothSize" placeholder="选择号码">
+              <el-option label="S" value="s"></el-option>
+              <el-option label="M" value="m"></el-option>
+              <el-option label="L" value="l"></el-option>
+              <el-option label="XL" value="xl"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="移动电话" prop="phone">
+            <el-input v-model="ruleForm.phone"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
+          </el-form-item>
         </el-form>
-        <div slot="footer" class="dialog-footer">
-          <el-button @click="dialogFormVisible = false">取 消</el-button>
-          <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
-        </div>
       </el-dialog>
       <div id="detail">
         <hr>
@@ -76,27 +106,73 @@
 export default {
   data () {
     return {
-      name: '足球錦標賽',
-      time: '12/32 14:30',
+      name: '2017年清华大学校园马拉松',
+      competitionTime: '2017／03／06 14：00',
+      registerTime: '2017-03-02  --  2017-03-05',
       location: '操场',
-      catagory: '⚽️',
+      catagory: '径赛',
       host: '计算机系',
       qualification: '计算机系同学',
       attend: '30',
       date: '1',
       detail: '足球比赛足球比赛足球比赛足球比赛足球比赛足球比赛足球比赛足球比赛足球比赛足球比赛足球比赛足球比赛足球比赛足球比赛足球比赛足球比赛足球比赛足球比赛足球比赛足球比赛足球比赛足球比赛',
       dialogFormVisible: false,
-      form: {
+      ruleForm: {
+        department: '',
         name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+        gender: '',
+        idNumber: '',
+        studentId: '',
+        birth: '',
+        clothSize: '',
+        phone: ''
+      },
+      rules: {
+        department: [
+          { required: true, message: '请选择院系', trigger: 'change' }
+        ],
+        name: [
+          { required: true, message: '请输入名字', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        gender: [
+          { required: true, message: '请选性别', trigger: 'change' }
+        ],
+        idNumber: [
+          { required: true, message: '请输入身份证号码', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        studentId: [
+          { required: true, message: '请输入学号', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        birth: [
+          { required: true, message: '请选择生日', trigger: 'change' }
+        ],
+        clothSize: [
+          { required: true, message: '请选择衣服号码', trigger: 'change' }
+        ],
+        phone: [
+          { required: true, message: '请输入学号', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ]
       },
       formLabelWidth: '120px'
+    }
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
     }
   }
 }
