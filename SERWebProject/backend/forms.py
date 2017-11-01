6a56f4a3ab71c3351b5e98856bdb9b7244709921
叶泽轩 <ye-zx14@mails.tsinghua.edu.cn>
2017-11-01 09:18:56 +0800

    add UserInfoForm user-info-submit interface view and url config #36

--- SERWebProject/backend/forms.py
+++ SERWebProject/backend/forms.py
@@ -12,3 +12,9 @@ class RegisterForm(UserCreationForm):
     def account9auth():
         redirect('https://accounts.net9.org/api/authorize')
 
+
+class UserInfoForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ("student_id", "id_card", "gender", "birth_date", "reading_degree", "faculty", "class_id",
+                  "clothes_size", "email", "cellphone_num", "dormitory")
