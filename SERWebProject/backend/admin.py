b6b84f86923dae5bfc8cdbdede4088e5f29d4f26
叶泽轩 <yezx5@yonyou.com>
2017-11-09 00:06:29 +0800

    fix admin conflict

--- SERWebProject/backend/admin.py
+++ SERWebProject/backend/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project, User, ProjectRegisterRelationship, UserInfo
+from .models import Project, User, ProjectRegisterRelationship, UserInfo, Group, Membership
 
 # Register your models here.
 
@@ -7,4 +7,7 @@ admin.site.register(Project)
 admin.site.register(User)
 admin.site.register(UserInfo)
 admin.site.register(ProjectRegisterRelationship)
+admin.site.register(Group)
+admin.site.register(Membership)
+
 
