ccf0d51fb0c83d15ce96096191b0f493c0a8bbb3
王斯右 <wsy17@mails.tsinghua.edu.cn>
2017-11-15 05:12:20 +0800

    update grade table with new api data #73

--- SERWebProject/frontend/src/components/CompetitionInfo/grades.vue
+++ SERWebProject/frontend/src/components/CompetitionInfo/grades.vue
@@ -2,29 +2,42 @@
   <el-table
     :data="tableData"
     style="width: 100%"
-    :default-sort = "{prop: 'grade', order: 'ascending'}"
+    :default-sort = "{prop: 'rank', order: 'ascending'}"
     >
     <el-table-column
-      prop="fields.grade"
+      prop="fields.rank"
       label="名次"
       width="180">
     </el-table-column>
-    <el-table-column
+    <el-table-column v-if="group === false"
       prop="fields.register_name"
       label="姓名"
       width="180">
     </el-table-column>
-    <el-table-column
+    <el-table-column v-else
+      prop="fields.group_name"
+      label="隊名"
+      width="180">
+    </el-table-column>
+    <el-table-column v-if="group === false"
       prop="fields.registered_project_name"
       label="項目">
     </el-table-column>
-    <el-table-column
+    <el-table-column v-else
+      prop="fields.team_creator_name"
+      label="隊長">
+    </el-table-column>
+    <el-table-column v-if="group === false"
       prop="fields.student_id"
-      label="学号">
+      label="學號">
+    </el-table-column>
+    <el-table-column v-else
+      prop="fields.teammate_num"
+      label="隊員人數">
     </el-table-column>
     <el-table-column
-      prop="fields.other"
-      label="備註">
+      prop="fields.grade"
+      label="成績">
     </el-table-column>
   </el-table>
 </template>
@@ -40,9 +53,11 @@
             regitster_name: '',
             registered_project_name: '',
             other: '--',
-            student_id: ''
+            student_id: '',
+            if_group_project: false
           }
-        }]
+        }],
+        group: false
       }
     },
     created: function () {
@@ -55,6 +70,9 @@
           console.log(res)
           if (res.error_num === 0) {
             this.tableData = res['list']
+            if (this.tableData[0].fields.if_group_project === true) {
+              this.group = true
+            }
             console.log(this.tableData)
           } else {
             this.$message.error('获取项目列表失败"')
