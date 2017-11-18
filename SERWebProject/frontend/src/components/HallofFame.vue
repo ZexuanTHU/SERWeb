7a46ce27d6433b7ca2b1d2df675f89e3b3ee9c57
温家尊 <jiazunvoon@hotmail.com>
2017-11-18 14:42:16 +0800

    support data from backend #87

--- SERWebProject/frontend/src/components/HallofFame.vue
+++ SERWebProject/frontend/src/components/HallofFame.vue
@@ -33,9 +33,12 @@
                   <el-dialog
                     :visible.sync="dialogVisible"
                     size="medium">
+                    <h1> {{ name }}</h1>
                     <div>
                       <div style="float: right; margin-bottom: 100px">
-                        <img :src="getImage(field.fields.HOF_image)" v-bind:alt="field" style="height: 300px;">
+                        <div v-if="loaded">
+                          <img :src="getImage2(hof_img)" style="height: 300px;">
+                        </div>
                       </div>
                       <div style="margin-right: 600px">
                         <el-tabs v-model="activeName" @tab-click="handleClick">
@@ -45,20 +48,12 @@
                             <li> 班级 : {{ class_id }} </li>
                             <li> 擅长项目 : {{ expertise}} </li>
                           </el-tab-pane>
-                          <!--<el-tab-pane label="个人简介" name="second">-->
-                          <!--{{ intro }}-->
-                          <!--</el-tab-pane>-->
-                          <!--<el-tab-pane label="个人荣誉" name="third">-->
-                          <!--<ul v-for="award in awards">-->
-                          <!--<li> {{ award.first }} </li>-->
-                          <!--<li> {{ award.second }} </li>-->
-                          <!--<li> {{ award.third }} </li>-->
-                          <!--<li> {{ award.fourth }} </li>-->
-                          <!--<li> {{ award.fifth }} </li>-->
-                          <!--<li> {{ award.sixth }} </li>-->
-                          <!--<li> {{ award.seventh }} </li>-->
-                          <!--</ul>-->
-                          <!--</el-tab-pane>-->
+                          <el-tab-pane label="个人简介" name="second">
+                          {{ intro }}
+                          </el-tab-pane>
+                          <el-tab-pane label="个人荣誉" name="third">
+                          {{ honor }}
+                          </el-tab-pane>
                         </el-tabs>
                       </div>
                     </div>
@@ -84,12 +79,15 @@
         openedPanel: '0',
         activeName: 'first',
         dialogVisible: false,
+        loaded: false,
         name: '',
         gender: '',
         selected_year: '',
         class_id: '',
         expertise: '',
         hof_img: '',
+        intro: '',
+        honor: '',
         hall_of_fame_data: [{
           fields: [{
             HOF_name: '',
@@ -102,35 +100,23 @@
             HOF_image: ''
           }],
           pk: ''
-        }],
-        items: [{
-          name: '李晨曦',
-          gender: '男',
-          grade: '2013级',
-          class: 'xx 班',
-          expertise: '中长跑'
-        }],
-        intro: '李晨曦，清华大学计算机系2013级博士生，马拉松国家二级运动员，曾创下北京国际马拉松赛全程2小时51分18秒完赛的个人最佳成绩，42.195千米的赛道，平均每千米用时约4分4秒，被称为“清华马拉松第一人”，原虎扑翻译团版主、虎扑新声编辑。',
-        awards: [{
-          first: '2016年9月以2小时51分18秒创造清华大学在校普通生的马拉松最好成绩',
-          second: '2016年5月代表清华大学参加New Balance中国大学生校园路跑接力赛总决赛获得冠军',
-          third: '2016年9月代表清华大学参加耐克高校精英赛公路接力赛总决赛获得冠军并创造本届赛事普通生单棒最好成绩',
-          fourth: '2016年4月参加清华大学“马约翰杯”学生田径运动会，获得男子甲组10000米和5000米双料冠军、4*800米接力第四、4*400米接力第五、师生异程接力第六',
-          fifth: '2014年清华大学“马约翰杯”学生田径运动会万米接力冠军队成员、4*800米接力冠军',
-          sixth: '2015年2月翻译出版了吉列姆.巴拉格所著的球星传记《梅西》',
-          seventh: '2016年10月翻译出版了罗兰.拉赞比所著的球星传记《乔丹》'
         }]
       }
     },
     methods: {
+      getData () {
+        this.loaded = true
+      },
       itemClicked: function (item) {
+        this.getData()
         this.name = item.fields.HOF_name
         this.gender = item.fields.HOF_gender
         this.selected_year = item.fields.HOF_selected_year
         this.class_id = item.fields.HOF_class_id
         this.expertise = item.fields.HOF_expertise
         this.hof_img = item.fields.HOF_image
-        console.log(this.hof_img)
+        this.intro = item.fields.HOF_introduction
+        this.honor = item.fields.HOF_honor
         this.dialogVisible = true
       },
       openPanel: function (index) {
@@ -143,10 +129,13 @@
         var images = require.context('../assets/', true, /\.jpg$/)
         return images('./' + index)
       },
+      getImage2 (index) {
+        var images = require.context('../assets/', true, /\.jpg$/)
+        return images('./' + index)
+      },
       hall_of_fame_request () {
         this.$http.get('http://127.0.0.1:8000/api/hall_of_fame_request').then((response) => {
           var res = JSON.parse(response.bodyText)
-          console.log(res)
           if (res.error_num === 0) {
             this.hall_of_fame_data = res['list']
           } else {
@@ -178,6 +167,9 @@
     text-decoration: none;
   }
 
+  [v-cloak] {
+    display: none;
+  }
   #leftnav {
     margin-top: 40px;
     width: 200px;
